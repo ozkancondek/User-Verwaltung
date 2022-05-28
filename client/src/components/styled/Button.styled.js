9081fcc3635a232b1e635a336239d9c12a7ff360
ozkancondek <ozkncndek@gmail.com>
2022-05-28 19:47:04 +0200

    FE:Styled Button created

---
+++ client/src/components/styled/Button.styled.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  border-radius: 5px;
+  border: 2px solid #e7e7e7;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
+  cursor: pointer;
+  font-size: 22px;
+  font-weight: 700;
+  padding: 15px 25px;
+  background-color: ${({ bg }) => bg || "  aqua"};
+  color: ${({ color }) => color || "black"};
+  &:hover {
+    opacity: 0.9;
+    transform: scale(0.98);
+  }
+`;
