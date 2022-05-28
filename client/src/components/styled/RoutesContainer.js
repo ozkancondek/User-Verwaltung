9a9004ced3a699627df97b5132463c13065d7077
ozkancondek <ozkncndek@gmail.com>
2022-05-28 22:18:33 +0200

    FE:Router.js and related style created

---
+++ client/src/components/styled/RoutesContainer.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+/* body height */
+export const RoutesContainer = styled.div`
+  min-height: 90vh;
+`;
---
+++ client/src/routes/Router.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { RoutesContainer } from "../components/styled/RoutesContainer";
+import { Home } from "../pages/Home";
+import { Users } from "../pages/Users";
+import { User } from "../pages/User";
+import { About } from "../pages/About";
+import Navbar from "../components/Navbar";
+
+export const Router = () => {
+  return (
+    <React.Fragment>
+      <Navbar />
+      <RoutesContainer>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/user/:id" element={<User />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </RoutesContainer>
+
+      {/*     <Footer /> */}
+    </React.Fragment>
+  );
+};
