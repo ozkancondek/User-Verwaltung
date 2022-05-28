f0a89708eb984cc6f1f49310f5cf8ee4b8fe723f
ozkancondek <ozkncndek@gmail.com>
2022-05-29 01:36:00 +0200

    FE:Connect button at Home.js to users page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,12 +3,18 @@ import { FlexContainer } from "../components/styled/FlexContainer.styled";
 import { Button } from "../components/styled/Button.styled";
 import bg from "../assets/bg.avif";
 import { useTheme } from "../providers/ThemeProvider";
+import { useNavigate } from "react-router-dom";
 
 export const Home = () => {
+  const navigate = useNavigate();
   const { panel } = useTheme();
   return (
     <FlexContainer width="100%" height="95vh" img={bg}>
-      <Button width="200px" bg={panel.bg.grey}>
+      <Button
+        width="200px"
+        bg={panel.bg.grey}
+        onClick={() => navigate("/users")}
+      >
         Get Start
       </Button>
     </FlexContainer>
