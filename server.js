2fc45c0c9e03f6a2be24866c825e24d97e5053df
ozkancondek <ozkncndek@gmail.com>
2022-05-28 17:15:37 +0200

    BE:Install concurrently package to star both server and client side

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//dotenv configuration package
 require("dotenv").config();
 
 const express = require("express");
@@ -6,18 +7,30 @@ const app = express();
 
 const router = require("./routes/router");
 
+//call connectDB function
 const connectDB = require("./utils/database/connectDB");
 
-//trigger connect to db function
+//trigger connectDB function to connect db
 connectDB();
 
 //mw for req.body
 app.use(express.json());
 
+//production
+if (process.env.NODE_ENV === "production") {
+  //mw for using files in build folder on client side
+  app.use(express.static("client/build"));
+  // index.html for all get requests
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 //forward every request to router
 app.use("/api", router);
 
-//listen to server
-app.listen(process.env.PORT, () => {
-  console.log("I am listening on port  " + process.env.PORT);
+//listen to server on port 5000
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`I am listening on port ${port}`);
 });
