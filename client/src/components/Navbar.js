cba29c1f8e16527b2d3431ea717e9bb5a41c57d6
ozkancondek <ozkncndek@gmail.com>
2022-05-29 00:07:17 +0200

    FE:Navbar component and styles created

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import {
+  NavbarContainer,
+  LeftContainer,
+  RightContainer,
+  NavbarExtendedContainer,
+  NavbarInnerContainer,
+  NavbarLinkContainer,
+  NavbarLink,
+  OpenLinksButton,
+  NavbarLinkExtended,
+  LogoContainer,
+} from "./styled/Navbar.styled";
+import { MdOutlineDarkMode } from "react-icons/md";
+import { BsFillSunFill } from "react-icons/bs";
+
+import { useTheme } from "../providers/ThemeProvider";
+
+function Navbar() {
+  const { panel, isDark, setIsDark } = useTheme();
+  const [extendNavbar, setExtendNavbar] = useState(false);
+
+  return (
+    <NavbarContainer extendNavbar={extendNavbar} theme={panel} isdark={isDark}>
+      <NavbarInnerContainer>
+        <LeftContainer>
+          <NavbarLinkContainer>
+            <NavbarLink to="/" theme={panel} isdark={isDark}>
+              Home
+            </NavbarLink>
+            <NavbarLink to="/users" theme={panel} isdark={isDark}>
+              Users
+            </NavbarLink>
+
+            <NavbarLink to="/about" theme={panel} isdark={isDark}>
+              About Us
+            </NavbarLink>
+            <OpenLinksButton
+              theme={panel}
+              isdark={isDark}
+              onClick={() => {
+                setExtendNavbar((val) => !val);
+              }}
+            >
+              {extendNavbar ? <>&#10005;</> : <> &#8801;</>}
+            </OpenLinksButton>
+          </NavbarLinkContainer>
+        </LeftContainer>
+        <RightContainer>
+          <LogoContainer
+            theme={panel}
+            isdark={isDark}
+            onClick={() => setIsDark((prev) => !prev)}
+          >
+            {isDark ? (
+              <BsFillSunFill size={50} />
+            ) : (
+              <MdOutlineDarkMode size={50} />
+            )}
+          </LogoContainer>
+        </RightContainer>
+      </NavbarInnerContainer>
+      {extendNavbar && (
+        <NavbarExtendedContainer theme={panel}>
+          <NavbarLinkExtended to="/" theme={panel} isdark={isDark}>
+            Home
+          </NavbarLinkExtended>
+          <NavbarLinkExtended to="/users" theme={panel} isdark={isDark}>
+            Users
+          </NavbarLinkExtended>
+
+          <NavbarLinkExtended to="/about" theme={panel} isdark={isDark}>
+            About Us
+          </NavbarLinkExtended>
+        </NavbarExtendedContainer>
+      )}
+    </NavbarContainer>
+  );
+}
+
+export default Navbar;
---
+++ client/src/components/styled/Navbar.styled.js
@@ -0,0 +1,95 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const NavbarContainer = styled.nav`
+  width: 100%;
+  height: ${({ extendNavbar }) => (extendNavbar ? "45vh" : "80px")};
+
+  background-color: ${({ theme, isdark }) =>
+    isdark ? theme.bg.light : theme.bg.dark};
+  display: flex;
+  flex-direction: column;
+  ${({ theme }) => theme.screen.medium_min} {
+    height: 80px;
+  }
+`;
+
+export const LeftContainer = styled.div`
+  flex: 70%;
+  display: flex;
+  align-items: center;
+  padding-left: 5%;
+`;
+
+export const RightContainer = styled.div`
+  flex: 30%;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding-right: 40px;
+`;
+
+export const NavbarInnerContainer = styled.div`
+  width: 100%;
+  height: 80px;
+  display: flex;
+`;
+
+export const NavbarLinkContainer = styled.div`
+  display: flex;
+`;
+
+export const NavbarLink = styled(Link)`
+  color: ${({ theme, isdark }) =>
+    isdark ? theme.text.primary : theme.text.secondary};
+  font-size: x-large;
+  font-family: Arial, Helvetica, sans-serif;
+  text-decoration: none;
+  margin: 10px;
+  ${({ theme }) => theme.screen.medium_max} {
+    display: none;
+  }
+`;
+
+export const NavbarLinkExtended = styled(Link)`
+  color: ${({ theme, isdark }) =>
+    isdark ? theme.text.primary : theme.text.secondary};
+  font-size: x-large;
+  font-family: Arial, Helvetica, sans-serif;
+  text-decoration: none;
+  margin: 10px;
+`;
+
+export const LogoContainer = styled.div`
+  margin: 10px;
+  max-width: 180px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  color: ${({ theme, isdark }) =>
+    isdark ? theme.text.primary : theme.text.secondary};
+`;
+
+export const OpenLinksButton = styled.button`
+  width: 70px;
+  height: 50px;
+  background: none;
+  border: none;
+  color: ${({ theme, isdark }) =>
+    isdark ? theme.text.primary : theme.text.secondary};
+  font-size: 45px;
+  cursor: pointer;
+  ${({ theme }) => theme.screen.medium_min} {
+    display: none;
+  }
+`;
+
+export const NavbarExtendedContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  ${({ theme }) => theme.screen.medium_min} {
+    display: none;
+  }
+`;
